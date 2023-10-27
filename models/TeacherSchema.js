8ddfff533f66cab62ebaccfc828a9a48c45fbe03
HASHEBUR RAHMAN <mdhasheburrahman3@gmail.com>
2023-10-27 13:09:49 +0600

    Teacher data update

---
+++ models/TeacherSchema.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const TeacherSchema = new mongoose.Schema(
+
+
+    {
+        name: {
+            type: String,
+            required: true,
+
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+        },
+        // image:
+        // {
+        //     type: String,
+        //     required: false,
+        // },
+        gender: {
+            type: String,
+            required: true,
+        },
+
+        age: {
+            type: Number,
+            required: true,
+        },
+        position: {
+            type: String,
+            required: true,
+        },
+        dateOfBirth: {
+            type: String,
+            required: true,
+        },
+        education: {
+            type: String,
+            required: true,
+        },
+        idNumber: {
+            type: Number,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Teacher", TeacherSchema);
--- routes/Index.js
+++ routes/Index.js
@@ -1,13 +1,15 @@
 const studentRoutes = require("./student")
+const teacherRoutes = require("./teacher")
 
- 
+
 
 
 const routes = (app) => {
     studentRoutes(app)
-     
-    
-     
+    teacherRoutes(app)
+
+
+
 
 }
 
